e2dfe8dde8559ef4662e982df66ed54a6de039af
Talvin <talvintawiah@gmail.com>
2022-04-06 17:06:12 -0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 const _url = 'https://pub.dev/packages/url_launcher';
+
+var size = const SizedBox(height: 10);
 void main() {
   runApp(const MyApp());
 }
@@ -14,10 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-      
-        primarySwatch: Colors.green,
-      ),
+      theme: ThemeData.light(),
       home: Scaffold(
         backgroundColor: Colors.grey,
         appBar:AppBar(
@@ -44,35 +43,51 @@ class MyApp extends StatelessWidget {
             height: 180.0,
             width: 150.0,
        
-            child: Column(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
            crossAxisAlignment: CrossAxisAlignment.center,
            children:  [
-             const Padding(padding: EdgeInsets.all(5.0)),
-             const Text("Hello",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 23.0)),
-             const SizedBox(height: 5.0,),
-             const Text("Please login to Your Account",style:TextStyle(color: Colors.grey)),
+               const Padding(padding: EdgeInsets.all(5.0)),
+               const Text("Hello",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 23.0,color: Colors.black)),
+               const SizedBox(height: 5.0,),
+               const Text("Please login to Your Account",style:TextStyle(color: Colors.grey)),
+                const SizedBox(height: 10.0,),
+              TextFormField(
+                textAlign: TextAlign.start,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  
+                  hintText: 'Enter your email',
+                  suffixIcon: Icon(Icons.email),
+                ),
+              ),
               const SizedBox(height: 10.0,),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-                
-                hintText: 'Enter your email',
-                prefixIcon: Icon(Icons.email),
+               TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter secure password',
+                  suffixIcon: Icon(Icons.password_sharp)
+                ),
               ),
-            ),
-            const SizedBox(height: 10.0,),
-             TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                hintText: 'Enter secure password',
-                prefixIcon: Icon(Icons.password_sharp)
+              const SizedBox(height: 10.0,),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              // textBaseline: TextBaseline.,
+              children: [
+                size,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(onTap: _launchURL, child: const Text('Forget Password')),
+                ),
+              ],
+          ),
+       ElevatedButton(onPressed: _launchURL, child: 
+       const Text('Sign up'),style: ButtonStyle(overlayColor: MaterialStateProperty.all(Colors.pink),backgroundColor: MaterialStateProperty.all(Colors.amber))
+       )],
               ),
-            ),
-            const SizedBox(height: 10.0,),
-         const ElevatedButton(onPressed: _launchURL, child: Text('Forget Password'))
-           ],
             ),
         
           ),
