082924bdb2193940153c34bd734fd6ea4b46988c
Talvin <talvintawiah@gmail.com>
2022-04-14 01:19:22 -0400

    /

--- lib/todo.dart
+++ lib/todo.dart
@@ -4,8 +4,9 @@ var size = const SizedBox(height: 10);
 class Todo extends StatelessWidget {
    Todo({ Key? key }) : super(key: key);
 
-final  controller1 = TextEditingController();
-final  controller2 = TextEditingController();
+// final  controller1 = TextEditingController();
+// final  controller2 = TextEditingController();
+
 
 
   @override
@@ -13,39 +14,62 @@ final  controller2 = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
-        actions: const [],
-        title: const Text(' Add new To-Do-List')
+       
+        title: const Text('To-Do-List'),
+         actions:  [
+           GestureDetector(
+             onTap: (() { }),
+             child: const Icon(Icons.add))
+         ],
       ),
       body:const Todoos() ,
+    
     );
   }
 }
 
 class Todoos extends StatefulWidget {
   const Todoos({ Key? key }) : super(key: key);
+  
 
   @override
   State<Todoos> createState() => _TodoosState();
 }
 
 class _TodoosState extends State<Todoos> {
+
+  final List<String> entries = <String>['yo','me','pull'];
+final List<int> colorCodes = <int>[100,200,600];
+bool _value = false;
   @override
   Widget build(BuildContext context) {
     return  Column(
       children: [
-        Row(
-          children:  [
-           const Pop(),
-           size,
-          ],
-        ),
-        Form(child:
-         TextFormField(
-
-         ))
-      ],
+    Expanded(
+   
+      child: ListView.builder(
+           padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder:(context, index) =>
+           Container(
+                   height: 50,
+                   color: Colors.blue[colorCodes[index]],
+                   child:  Row(
+                     children: [
+                     const MyStatefulWidget(),
+                        Text(entries[index])
+                     ],
+                     
+           ),
+           )
+    )
+      
+    )],
     );
   }
+
+ 
+
 }
 
 class Pop extends StatelessWidget {
@@ -64,4 +88,27 @@ class Pop extends StatelessWidget {
       
     );
   }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+ const MyStatefulWidget({Key? key}) : super(key: key);
+
+ @override
+ State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+ bool isChecked = false; // This holds the state of the checkbox, we call setState and update this whenever a user taps the checkbox
+
+ @override
+ Widget build(BuildContext context) {
+   return Checkbox(
+     value: isChecked,
+     onChanged: (bool? value) { // This is where we update the state when the checkbox is tapped
+       setState(() {
+         isChecked = value!;
+       });
+     },
+   );
+ }
 }
